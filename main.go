bb4a8731e3706ed13733f322af11f4975b2ea68c
Marius P <marius.pop@spyhce.com>
2019-09-19 03:19:35 +0300

    (patch) update commit msg

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"strings"
 
 	"semtag/pkg/docker"
 	"semtag/pkg/git"
@@ -15,8 +16,13 @@ var (
 	out    string
 	suffix string
 	prefix string
-	dryRun bool
 )
+var (
+	dryRun  bool
+	skipInc bool
+)
+
+const commitMsgVerBump = "ver bump"
 
 func parseFlags() {
 	flag.StringVar(&tag, "tag", "", "tag choices: file | git | docker")
@@ -24,7 +30,9 @@ func parseFlags() {
 	flag.StringVar(&out, "out", "", `output: can be either 1) a docker repository or 2) the pattern for the file version (e.g. "version='%s',")`)
 	flag.StringVar(&suffix, "suffix", "", `if set, append the suffix to the version number (e.g. "0.1.0-rc")`)
 	flag.StringVar(&prefix, "prefix", "", `if set, append the prefix to the version number (e.g. "api-0.1.0")`)
+
 	flag.BoolVar(&dryRun, "dry-run", false, "if true, only print the object(s) that would be sent, without sending the data")
+	flag.BoolVar(&skipInc, "skip-inc", false, "if true, do not increment the version number")
 	flag.Parse()
 
 	if dryRun {
@@ -40,6 +48,13 @@ func main() {
 
 	switch tag {
 	case "file":
+		out, err := git.GetLastCommitNames(-1)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.Contains(*out, commitMsgVerBump) {
+			log.Fatal("version already incremented ; exiting")
+		}
 		tagFile(nextVer)
 	case "git":
 		tagGit(nextVer)
@@ -56,6 +71,9 @@ func getVersions() (*version.Version, *version.Version) {
 	ver.Suffix = suffix
 	ver.Prefix = prefix
 	ver = *ver.GetLatest()
+	if skipInc {
+		return &ver, &ver
+	}
 	log.Println("current version:", ver.String())
 	nextVer = *ver.GetLatest()
 	if err := nextVer.IncrementAuto(); err != nil {
@@ -73,14 +91,14 @@ func tagFile(ver *version.Version) {
 	}
 	newContents := f.ReplaceSubstring()
 	log.Println("tagging file:", f, "\n", *newContents)
+	f.Write(newContents)
 	if !dryRun {
-		f.Write(newContents)
 		git.Add(in)
 		msg, err := git.GetLastCommitNames(-1)
 		if err != nil {
 			log.Fatal(err)
 		}
-		git.Commit(*msg + " [skip ci]")
+		git.Commit(commitMsgVerBump + ": " + *msg)
 		git.Push()
 	}
 }
